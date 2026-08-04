f825a92c071379a253466b11a71b8077cf01adfd
aPerson906 <alfonso.delgado@education.nsw.gov.au>
2026-08-04 15:06:53 +1000

    Push 7
    Started on my coding stage. I made the basic outline of what my code will do and added comments on what it is supposed to do.

--- main/main.py
+++ main/main.py
@@ -1,1 +1,21 @@
-print('Hi')
+# light_levels function
+# motion_detect function
+# timer function
+
+def motion_detect():
+    motion_sensor = input ("insert")
+    while motion_sensor == True: #while motion detetector senses motion
+        print("LED on") #Put's light/LED on
+        timer()
+    else:
+        light_levels() #otherwise go back and check light levels
+
+def light_levels():
+    light_sensor = input('insert')
+    while light_sensor == True: #while light sensor decects light
+        light_levels() #loop back
+    else:
+        motion_detect() #send signal to motion dectector to check motion
+
+def timer():
+    print("hi")
