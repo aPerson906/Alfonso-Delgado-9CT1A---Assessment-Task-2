a4285060bf9a3923d4671d41801e324024bd1180
aPerson906 <alfonso.delgado@education.nsw.gov.au>
2026-08-06 11:02:16 +1000

    Push 8
    Worked on my code more. Set up what I needed and made my code more efficient.

--- main/main.py
+++ main/main.py
@@ -1,21 +1,31 @@
+from machine import Pin, ADC
+import time
+
+
+light_sensor = ADC(26) # LDR (light sensor) 
+motion_sensor = Pin(15, Pin.IN) # PIR (motion sensor)
+led = Pin(16, Pin.OUT) # LED (light)
+
+
+
 # light_levels function
+def light():
+    if light_sensor <= 50:
+        motion_detect()
+    else:
+        light()
+
+# timer funtion 
+def seconds():
+    while time <= 30:
+        time + 1
+        time.sleep(1)
+    time.sleep(2) # A 2 second delay
+    light()
+
 # motion_detect function
-# timer function
-
 def motion_detect():
-    motion_sensor = input ("insert")
-    while motion_sensor == True: #while motion detetector senses motion
-        print("LED on") #Put's light/LED on
-        timer()
+    if motion_sensor.value() == 1:  #If motion_sensor value = 1 then motion is detected
+        seconds()
     else:
-        light_levels() #otherwise go back and check light levels
-
-def light_levels():
-    light_sensor = input('insert')
-    while light_sensor == True: #while light sensor decects light
-        light_levels() #loop back
-    else:
-        motion_detect() #send signal to motion dectector to check motion
-
-def timer():
-    print("hi")
+        light()
