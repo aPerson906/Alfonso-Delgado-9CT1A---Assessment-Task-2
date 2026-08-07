245b5ef9af46925866db0b149adb3aed8ee325a2
aPerson906 <alfonso.delgado@education.nsw.gov.au>
2026-08-07 12:23:31 +1000

    Push 9
    Finished the code and started thinking about the basic circuitry. I will have to come back to this to fix mistakes and optimise code.

--- main/main.py
+++ main/main.py
@@ -7,25 +7,35 @@ motion_sensor = Pin(15, Pin.IN) # PIR (motion sensor)
 led = Pin(16, Pin.OUT) # LED (light)
 
 
-
-# light_levels function
+#LIght sensor function
 def light():
-    if light_sensor <= 50:
+    light_level = light_sensor.read_u16()  
+
+    if light_level <= 50: # Reads light levels if they are above 50 then move onto motion detect func
         motion_detect()
     else:
-        light()
+        led.off()   
 
 # timer funtion 
 def seconds():
-    while time <= 30:
-        time + 1
-        time.sleep(1)
-    time.sleep(2) # A 2 second delay
-    light()
+    led.on()
+
+    timer = 0
+    while timer < 30: #while the timer is less than 30 add 1 more second until it is 30 or above then turn LED off
+        time.sleep(1) # 1 second delay
+        timer += 1
+
+    led.off()
+
 
 # motion_detect function
 def motion_detect():
-    if motion_sensor.value() == 1:  #If motion_sensor value = 1 then motion is detected
+    if motion_sensor.value() == 1:   # #If motion_sensor value = 1 then motion is detected
         seconds()
     else:
-        light()
+        led.off()
+
+# Main program
+while True:
+    light()
+    time.sleep(0.1)
